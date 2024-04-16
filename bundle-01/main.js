c44fab7e1d49aeaac0f98f0917cea416a380d80b
matteocenci <matteocenci01@gmail.com>
2024-04-16 16:21:28 +0200

    feat: bundle 01

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -17,8 +17,15 @@ for (let i = 0; i > 5; i++) {
 
 
 // ESERCIZIO 2
+// function addIfEven(num) {
+    // if (num % 2 = 0) { //devono esserci due uguali non uno
+        // return num + 5;
+    // }
+    // return num;
+// }
+
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 == 0) {         // correzione
         return num + 5;
     }
     return num;
@@ -26,22 +33,46 @@ function addIfEven(num) {
 
 
 // ESERCIZIO 3
+// function loopToFive() {
+    // for (let i = 0, i < 5, i++) {     // ci devono essere ; e non solo ,
+        // console.log(i);
+    // }
+// }
+
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 5; i++) {        //correzione
         console.log(i);
     }
 }
 
-
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+// function displayEvenNumbers() {
+    // let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    // let evenNumbers = [];
+    // for (let i = 0; i < numbers.length - 1; i++;) {      //errore di punto e virgola posizionato male
+        // if (numbers % 2 = 0); {  //errore di due uguali
+            // evenNumbers.push(i);   //elemento i non è definito
+        // }
+        // return evenNumbers;
+    // }
+// }
+// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+
+
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length; i++); {      //errore di punto e virgola posizionato male
+        if (numbers % 2 === 0); {  //errore di due uguali
+            evenNumbers.push(numbers[i]);
         }
         return evenNumbers;
     }
+
+    
 }
-displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+displayEvenNumbers(); 
+
+console.log(numbers);
+// dovrebbe restituire [2,4,6,8]
